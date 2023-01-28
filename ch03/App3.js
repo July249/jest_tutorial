47efcfe71f834fffdbb9d690d83aacc71c319846
jihyunglee <jhlee7125@gmail.com>
2023-01-29 02:45:37 +0900

    ch03 비동기 코드 테스트 비활성화를 위한 주석처리 진행

--- ch03/App3.js
+++ ch03/App3.js
@@ -1,34 +1,34 @@
-const App3 = {
-  add: (num1, num2) => num1 + num2,
-  getName: (callback) => {
-    const name = "Mike";
-    setTimeout(() => {
-      callback(name);
-    }, 3000);
-  },
-  getNameWithErr: (callBack) => {
-    const name = "Mike";
-    setTimeout(() => {
-      // callback(name)
-      throw new Error("서버 에러...");
-    }, 3000);
-  },
-  getAge: () => {
-    const age = 30;
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(age);
-      }, 3000);
-    });
-  },
-  getAgeWithReject: () => {
-    const age = 30;
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        reject("error");
-      }, 3000);
-    });
-  },
-};
+// const App3 = {
+//   add: (num1, num2) => num1 + num2,
+//   getName: (callback) => {
+//     const name = "Mike";
+//     setTimeout(() => {
+//       callback(name);
+//     }, 3000);
+//   },
+//   getNameWithErr: (callBack) => {
+//     const name = "Mike";
+//     setTimeout(() => {
+//       // callback(name)
+//       throw new Error("서버 에러...");
+//     }, 3000);
+//   },
+//   getAge: () => {
+//     const age = 30;
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         resolve(age);
+//       }, 3000);
+//     });
+//   },
+//   getAgeWithReject: () => {
+//     const age = 30;
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         reject("error");
+//       }, 3000);
+//     });
+//   },
+// };
 
-module.exports = App3;
+// module.exports = App3;
--- ch03/App3.test.js
+++ ch03/App3.test.js
@@ -1,11 +1,11 @@
-const App3 = require("./App3");
+// const App3 = require("./App3");
 
-test("3초 후에 받아온 이름은 Mike", () => {
-  function callBack(name) {
-    expect(name).toBe("Mike");
-  }
-  App3.getName(callBack);
-});
+// test("3초 후에 받아온 이름은 Mike", () => {
+//   function callBack(name) {
+//     expect(name).toBe("Mike");
+//   }
+//   App3.getName(callBack);
+// });
 
 // test("3초 후에 받아온 이름은 Mike", () => {
 //   function callBack(name) {
@@ -18,13 +18,13 @@ test("3초 후에 받아온 이름은 Mike", () => {
 // => 비동기로 테스트가 진행되지 않는 것이 분명해!
 
 // done 이라고 하는 콜백 함수를 전달하면 해결됨!
-test("3초 후에 받아온 이름은 Mike", (done) => {
-  function callBack(name) {
-    expect(name).toBe("Mike");
-    done();
-  }
-  App3.getName(callBack);
-});
+// test("3초 후에 받아온 이름은 Mike", (done) => {
+//   function callBack(name) {
+//     expect(name).toBe("Mike");
+//     done();
+//   }
+//   App3.getName(callBack);
+// });
 // done이 호출되기 전까지는 종료되지 않고 기다리게 됨
 
 // API의 에러를 감지하고 싶은 경우에는 try-catch
@@ -47,28 +47,28 @@ test("3초 후에 받아온 이름은 Mike", (done) => {
 //   });
 // });
 // return을 빼먹으면 그냥 통과해버린다.
-test("3초 후에 받아온 나이는 30", () => {
-  return App3.getAge().then((age) => {
-    expect(age).toBe(30);
-  });
-});
+// test("3초 후에 받아온 나이는 30", () => {
+//   return App3.getAge().then((age) => {
+//     expect(age).toBe(30);
+//   });
+// });
 
 // Promise를 사용하는 방법 - resolves, rejects matcher 사용
-test("3초 후에 받아온 나이는 30", () => {
-  return expect(App3.getAge()).resolves.toBe(30);
-});
-test("3초 후에 에러가 발생합니다.", () => {
-  return expect(App3.getAgeWithReject()).rejects.toMatch("error");
-});
+// test("3초 후에 받아온 나이는 30", () => {
+//   return expect(App3.getAge()).resolves.toBe(30);
+// });
+// test("3초 후에 에러가 발생합니다.", () => {
+//   return expect(App3.getAgeWithReject()).rejects.toMatch("error");
+// });
 
 // Async - Await 방식
-test("3초 후에 받아온 나이는 30", async () => {
-  const age = await App3.getAge();
-  expect(age).toBe(30);
-});
-test("3초 후에 받아온 나이는 30", async () => {
-  await expect(App3.getAge()).resolves.toBe(30);
-});
-test("3초 후에 에러가 발생합니다.", async () => {
-  await expect(App3.getAgeWithReject()).rejects.toMatch("error");
-});
+// test("3초 후에 받아온 나이는 30", async () => {
+//   const age = await App3.getAge();
+//   expect(age).toBe(30);
+// });
+// test("3초 후에 받아온 나이는 30", async () => {
+//   await expect(App3.getAge()).resolves.toBe(30);
+// });
+// test("3초 후에 에러가 발생합니다.", async () => {
+//   await expect(App3.getAgeWithReject()).rejects.toMatch("error");
+// });
